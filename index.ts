f0e080d04f6d850b7f516426f4f5b4e0a96bdd80
Sehajdeep Singh <ssingh28_be24@thapar.edu>
2025-11-27 11:02:53 +0530

    first commit

---
+++ index.ts
@@ -0,0 +1,11 @@
+type person = {
+  name: string;
+  age: number;
+};
+
+const person1: person = {
+  name: "John",
+  age: 30,
+};
+
+console.log(person1);
---
+++ index2.ts
@@ -0,0 +1,7 @@
+let color:any = "Red";
+color = 10;
+color = true;
+console.log(color);
+color="Blue";
+color=color.toUpperCase();
+console.log(color);
