b3b322d8d835f990a666a46bcdfde64a450e197e
Sehajdeep Singh <ssingh28_be24@thapar.edu>
2026-01-30 09:08:34 +0530

    new commit

---
+++ 06_Function_return_Annotation/01_Function_return_Annotion.ts
@@ -0,0 +1,17 @@
+//Regular Function
+
+function double1(x:number){
+    return x*x
+}
+
+const res = double1(4)
+console.log(res)
+
+//Arrow Function
+
+const cubeNikalo=(y:number)=>{
+    return y**3
+}
+
+const cube = cubeNikalo(6)
+console.log(cube)
